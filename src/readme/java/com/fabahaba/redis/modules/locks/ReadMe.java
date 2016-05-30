9e0481cb0d5e428e19496598e8384c5087d632f5
jamespedwards42 <james.p.edwards42@gmail.com>
2016-05-30 16:29:51 -0700

    Add new file for last commit.

---
+++ src/readme/java/com/fabahaba/redis/modules/locks/ReadMe.java
@@ -0,0 +1,45 @@
+package com.fabahaba.redis.modules.locks;
+
+import java.util.UUID;
+
+import com.fabahaba.jedipus.cluster.Node;
+import com.fabahaba.jedipus.cmds.Cmds;
+import com.fabahaba.jedipus.executor.RedisClientExecutor;
+import com.fabahaba.redis.locks.AcquireReply;
+
+public final class ReadMe {
+
+  private ReadMe() {}
+
+  public static void main(final String[] args) {
+    cmodule();
+  }
+
+  static void cmodule() {
+    final String lockName = "MY_LOCK";
+    final String ownerId = UUID.randomUUID().toString();
+    final String pexpire = "2000";
+
+    try (final RedisClientExecutor rce =
+        RedisClientExecutor.startBuilding().create(() -> Node.create("localhost", 9736))) {
+
+      rce.accept(
+          client -> client.sendCmd(Cmds.MODULE, Cmds.MODULE_LOAD, "/redis/modules/locks.so"));
+
+      final AcquireReply acquireReply = rce
+          .apply(client -> client.sendCmd(RedisMutex.TRY_ACQUIRE, lockName, ownerId, pexpire), 1);
+
+      System.out.format("'%s' has lock '%s' for %dms.%n", acquireReply.getCurrentOwner(), lockName,
+          acquireReply.getTTLMillis());
+
+      final String releaseReply =
+          rce.apply(client -> client.sendCmd(RedisMutex.TRY_RELEASE, lockName, ownerId), 0);
+
+      if (releaseReply != null && releaseReply.equals(ownerId)) {
+        System.out.format("Lock was released by '%s'.%n", ownerId);
+      } else {
+        System.out.format("Lock was no longer owned by '%s'.%n", ownerId);
+      }
+    }
+  }
+}
